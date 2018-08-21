3adcd9daedf588d83951502b3be352fbab5f2762
JuanCarlosCV <A01702008@itesm.mx>
2018-08-20 22:40:21 -0500

    LAB #4.- Ejercicio 1 listo

--- Laboratorios/Lab 4/codigo.js
+++ Laboratorios/Lab 4/codigo.js
@@ -1,3 +1,14 @@
- Function(){
-     
- }
+
+ function ejer1(){
+var numLim = prompt("Introduzca un numero:");
+for (var n = 1; n <= numLim; n++) {
+var tablero = document.getElementById("tabla");
+    var fila=tablero.insertRow(n);
+    var  cel1 = fila.insertCell(0);
+    var cel2 = fila.insertCell(1);
+    var cel3 = fila.insertCell(2);
+    cel1.innerHTML = n;
+    cel2.innerHTML = n*n;
+    cel3.innerHTML = n*n*n;
+}    
+}
