47ad30a88952ea180d8d4fefc19f10e49a305267
JuanCarlosCV <A01702008@itesm.mx>
2018-08-26 23:37:40 -0500

    LAB#5.-revisando contraseñas

---
+++ Laboratorios/Lab 5/codigo.js
@@ -0,0 +1,12 @@
+function ver(){
+    var contraseña = document.forma.contra;
+    var contraVerifica = document.forma.verifica;
+     document.forma.prueba=contraseña;
+    if(contraseña==contraVerifica){
+       alert("iguales");
+       }else{
+           alert("no son");
+       }
+   
+ 
+}
