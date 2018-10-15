3f9c8698c6a50884bd7d61590a58bb8a6988047f
JuanCarlosCV <A01702008@itesm.mx>
2018-10-15 13:14:29 -0500

    Preguntas Lab 11

--- Laboratorios/LAB 11/sesion.php
+++ Laboratorios/LAB 11/sesion.php
@@ -1,36 +1,34 @@
 <?php
-
- if ($_POST["name"]=="luis" ) {
-            include("_header.html");
-            echo "Bienvenido ".$_POST["name"];
-            include("_bienvenido.html");
-            include("_footer.html");
-     echo "<script>";
-     echo "abrirsesion();";
-     echo "</script>";       
-     
-    
-            
-        } else {
-          header("location: index.php");
-        }
+include("_header.html");
+ 
         
 
 $first_name = $mail = $password = $r_pass  = "";
     
+
+
+
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $first_name = test_input($_POST["name"]);
       $mail = test_input($_POST["email"]);
       $password= test_input($_POST["pass"]);
       $r_pass = test_input($_POST["rpass"]);
         
+        
+        echo "<p>Usuario:  $first_name <p>";
+        echo "<p>Correo:  $mail <p>";
+        include("_bienvenido.html");
+        
     }
     
+
+
     function test_input($data) {
       $data = trim($data);
       $data = stripslashes($data);
       $data = htmlspecialchars($data);
       return $data;
     }
-
+include("_footer.html");
 ?>
---
+++ Laboratorios/LAB 13/index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+include("_header.html");
+     include("_section.html");
+    include("_footer.html");
+
+?>
